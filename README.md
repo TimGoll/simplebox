5f998a10990288ab659616f864df826de760d4a1
Tim Goll <tim@timgoll.de>
2018-12-20 18:44:18 +0100

    added documentation

---
+++ README.md
@@ -0,0 +1,35 @@
+# Simplebox
+Simplebox, a very lightweight fancy box alternative. This is still very much WIP.
+
+## Setup
+First of all you have to include all necessary files. It should look like this.
+```html
+<script type="text/javascript" src="../libs/simpleBox.js"></script>
+<link rel="stylesheet" href="../libs/simpleBox.css">
+```
+
+Now only one step is missing. You h ave to initialize the library.
+```javascript
+window.onload = function() {
+	simpleBox.init({offset: 50});
+}
+```
+These three lines set up everything. You can leave the init parameter empty and it defaults to `50px`. The offset is the min distance between the edge of the window and the edge of the image.
+
+## Creating Galleries
+A gallery is a slideshow of at least one image. There is no limit.
+```html
+<a simpleBox="gallery" gid="gal_1" class="simplebox_imageLink"> <img src="test_1.jpg"> </a>
+<a simpleBox="gallery" gid="gal_1" class="simplebox_imageLink"> <img src="test_2.jpg"> </a>
+<a simpleBox="gallery" gid="gal_1" class="simplebox_imageLink"> <img src="test_3.jpg"> </a>
+
+<a simpleBox="gallery" gid="gal_2" class="simplebox_imageLink"> <img src="test_1.jpg"> </a>
+<a simpleBox="gallery" gid="gal_2" class="simplebox_imageLink"> <img src="test_2.jpg"> </a>
+
+<a simpleBox="gallery" gid="gal_3" class="simplebox_imageLink"> <img src="test_3.jpg"> </a>
+```
+The argument `simpleBox="gallery"` has to be set in order for the library to detect the link. `gid` has to be set if you want to have more than one gallery. You can safely ignore it, if you don't plant on adding more galleries. It can be any unique string. The class `simplebox_imageLink` is just a small helper for you which adds a pointer cursor while hovering over the preview icon. There is no need on using this.
+
+## TODO
+- Load a description from a title or a designated description tag
+- set extra image tag in order to load a high res image if the icon is low res
--- libs/simpleBox.js
+++ libs/simpleBox.js
@@ -1,32 +1,32 @@
 var simpleBox = (function() {
 	//IDs / Classes
 	var IDs = {
-        MAINAREA           : "simplebox_mainarea",
-		MAINAREA_PREV      : "simplebox_mainarea_prev",
+		MAINAREA		   : "simplebox_mainarea",
+		MAINAREA_PREV	   : "simplebox_mainarea_prev",
 		MAINAREA_PREV_WRAP : "simplebox_mainarea_prev_wrapper",
-		MAINAREA_NEXT      : "simplebox_mainarea_next",
+		MAINAREA_NEXT	   : "simplebox_mainarea_next",
 		MAINAREA_NEXT_WRAP : "simplebox_mainarea_next_wrapper",
-        BACKGROUND         : "simplebox_background",
-        IMAGE              : "simplebox_image",
-        IMAGE_WRAP         : "simplebox_image_wrapper",
-        IMAGE_LOADING      : "simplebox_image_loading",
-        FOOTERSHADOW       : "simplebox_footer_shadow",
-        FOOTER             : "simplebox_footer",
-        SHADOW             : "simplebox_shadow"
+		BACKGROUND		   : "simplebox_background",
+		IMAGE			   : "simplebox_image",
+		IMAGE_WRAP		   : "simplebox_image_wrapper",
+		IMAGE_LOADING	   : "simplebox_image_loading",
+		FOOTERSHADOW	   : "simplebox_footer_shadow",
+		FOOTER			   : "simplebox_footer",
+		SHADOW			   : "simplebox_shadow"
 
 	};
 
-    window.onkeydown = function(event) {
-        var key = event.keyCode ? event.keyCode : event.which;
+	window.onkeydown = function(event) {
+		var key = event.keyCode ? event.keyCode : event.which;
 
-        if (key === 37) {        //left arrow
-            _prevImageEvent(null, false);
-        } else if (key === 39) { //right arrow
-            _nextImageEvent(null, false);
-        } else if (key === 27) { //esc
-            _closeGallery();
-        }
-    };
+		if (key === 37) {		//left arrow
+			_prevImageEvent(null, false);
+		} else if (key === 39) { //right arrow
+			_nextImageEvent(null, false);
+		} else if (key === 27) { //esc
+			_closeGallery();
+		}
+	};
 
 
 	var simpleBox = {
@@ -54,47 +54,47 @@ var simpleBox = (function() {
 		}
 	};
 
-    var _showGallery = function(event) {
-        _current_gid = event.path[1].getAttribute('gid');	//gallery id
-        _current_aid = event.path[1].id.substring(7);		//array id
+	var _showGallery = function(event) {
+		_current_gid = event.path[1].getAttribute('gid');	//gallery id
+		_current_aid = event.path[1].id.substring(7);		//array id
 		_openGallery();
 	};
 
-    var _openGallery = function(overlayShownOnOpen) {
+	var _openGallery = function(overlayShownOnOpen) {
 		if (overlayShownOnOpen === undefined)
 			overlayShownOnOpen = true;
 		
-        _currently_open = true;
+		_currently_open = true;
 
-        var src = _galleries[_current_gid][_current_aid];
-        _displayFrame(src, _current_gid, _current_aid, overlayShownOnOpen);
+		var src = _galleries[_current_gid][_current_aid];
+		_displayFrame(src, _current_gid, _current_aid, overlayShownOnOpen);
 
 		if (overlayShownOnOpen)
 			fade_out_timeout = setTimeout(_hideElements, 2500);
 
-        //image stuff
-        var image = document.getElementById(IDs.IMAGE);
-        image.onload = function() {
-            _changeImageMode(image);
-            _setNavAreas(image);
+		//image stuff
+		var image = document.getElementById(IDs.IMAGE);
+		image.onload = function() {
+			_changeImageMode(image);
+			_setNavAreas(image);
 			
 			//enable visibility of image
 			document.getElementById(IDs.IMAGE_LOADING).remove();
 			image.style.opacity = "1.0";
-        };
-    };
+		};
+	};
 
-    var _closeGallery = function() {
-        _currently_open = false;
+	var _closeGallery = function() {
+		_currently_open = false;
 
-        document.removeEventListener('mousemove', _mouseMoveEvent);
-        document.removeEventListener('click', _clickEvent);
+		document.removeEventListener('mousemove', _mouseMoveEvent);
+		document.removeEventListener('click', _clickEvent);
 
-        clearTimeout(fade_out_timeout);
+		clearTimeout(fade_out_timeout);
 
-        removeElement(IDs.BACKGROUND);
-        document.body.style.overflow = '';
-    };
+		removeElement(IDs.BACKGROUND);
+		document.body.style.overflow = '';
+	};
 
 	var _displayFrame = function (path, gid, aid, overlayShownOnOpen) {
 
@@ -108,186 +108,186 @@ var simpleBox = (function() {
 	};
 
 	var _createBox = function (nav_prev, nav_next, overlayShownOnOpen) {
-        document.addEventListener('mousemove', _mouseMoveEvent);
-        document.addEventListener('click', _clickEvent);
+		document.addEventListener('mousemove', _mouseMoveEvent);
+		document.addEventListener('click', _clickEvent);
 		
 		if (overlayShownOnOpen)
 			overlay_display = 'display: block;';
 		else
 			overlay_display = 'display: none;';
 
-		var simplebox_background    = createElement('div', IDs.BACKGROUND, IDs.BACKGROUND, '', document.body);
+		var simplebox_background	= createElement('div', IDs.BACKGROUND, IDs.BACKGROUND, '', document.body);
 		var simplebox_footer_shadow = createElement('div', IDs.FOOTERSHADOW, IDs.FOOTERSHADOW, overlay_display, simplebox_background);
-		var simplebox_footer        = createElement('div', IDs.FOOTER, IDs.FOOTER, overlay_display, simplebox_background);
+		var simplebox_footer		= createElement('div', IDs.FOOTER, IDs.FOOTER, overlay_display, simplebox_background);
 		var simplebox_image_wrapper = createElement('div', IDs.IMAGE_WRAP, IDs.IMAGE_WRAP, '', simplebox_background);
 			simplebox_image_wrapper.appendChild(_loading_image);
 			_loading_image.class = IDs.IMAGE_LOADING;
-			_loading_image.id    = IDs.IMAGE_LOADING;
+			_loading_image.id	 = IDs.IMAGE_LOADING;
 			_loading_image.style = "margin-left: 46px; margin-top:46px; opacity: .5;"
-		var simplebox_image         = createElement('img', IDs.IMAGE, '', 'opacity: 0.0;', simplebox_image_wrapper);
+		var simplebox_image		 = createElement('img', IDs.IMAGE, '', 'opacity: 0.0;', simplebox_image_wrapper);
 
 		if (nav_prev) {
-            var simplebox_prev_wrapper = createElement('div', IDs.MAINAREA_PREV_WRAP, IDs.MAINAREA_PREV_WRAP, overlay_display, simplebox_background);
-            var simplebox_prev         = createElement('div', IDs.MAINAREA_PREV, IDs.MAINAREA_PREV, '', simplebox_prev_wrapper);
-            simplebox_prev.addEventListener('click', _prevImageEvent);
-        }
+			var simplebox_prev_wrapper = createElement('div', IDs.MAINAREA_PREV_WRAP, IDs.MAINAREA_PREV_WRAP, overlay_display, simplebox_background);
+			var simplebox_prev		   = createElement('div', IDs.MAINAREA_PREV, IDs.MAINAREA_PREV, '', simplebox_prev_wrapper);
+			simplebox_prev.addEventListener('click', _prevImageEvent);
+		}
 		if (nav_next) {
 			var simplebox_next_wrapper = createElement('div', IDs.MAINAREA_NEXT_WRAP, IDs.MAINAREA_NEXT_WRAP, overlay_display, simplebox_background);
-            var simplebox_next = createElement('div', IDs.MAINAREA_NEXT, IDs.MAINAREA_NEXT, '', simplebox_next_wrapper);
-            simplebox_next.addEventListener('click', _nextImageEvent);
-        }
+			var simplebox_next         = createElement('div', IDs.MAINAREA_NEXT, IDs.MAINAREA_NEXT, '', simplebox_next_wrapper);
+			simplebox_next.addEventListener('click', _nextImageEvent);
+		}
 	};
 
-    var _changeImageMode = function(image) {
+	var _changeImageMode = function(image) {
 		var image_wrap = document.getElementById(IDs.IMAGE_WRAP);
 		
-        if (window.innerWidth / window.innerHeight <= image.width / image.height) {
-            image_wrap.className = 'simplebox_image_landscape';
-            image.className = 'simplebox_image_landscape';
+		if (window.innerWidth / window.innerHeight <= image.width / image.height) {
+			image_wrap.className = 'simplebox_image_landscape';
+			image.className      = 'simplebox_image_landscape';
 
-            image_wrap.style['width']       = (window.innerWidth - 2 * _offset) + "px";
-            image_wrap.style['margin-left'] = _offset + "px";
-            image_wrap.style['margin-top']  = (window.innerHeight - image.height) / 2 + "px";
-        } else {
-            image_wrap.className = 'simplebox_image_portrait';
-            image.className = 'simplebox_image_portrait';
+			image_wrap.style['width']	   = (window.innerWidth - 2 * _offset) + "px";
+			image_wrap.style['margin-left'] = _offset + "px";
+			image_wrap.style['margin-top']  = (window.innerHeight - image.height) / 2 + "px";
+		} else {
+			image_wrap.className = 'simplebox_image_portrait';
+			image.className      = 'simplebox_image_portrait';
 
-            image_wrap.style['height']      = (window.innerHeight - 2 * _offset) + "px";
-            image_wrap.style['margin-top']  = _offset + "px";
-            image_wrap.style['margin-left'] = (window.innerWidth - image.width) / 2 + "px";
-        }
-    };
+			image_wrap.style['height']	  = (window.innerHeight - 2 * _offset) + "px";
+			image_wrap.style['margin-top']  = _offset + "px";
+			image_wrap.style['margin-left'] = (window.innerWidth - image.width) / 2 + "px";
+		}
+	};
 
-    var _setNavAreas = function(image) {
-        var width  = image.width;
-        var height = image.height;
+	var _setNavAreas = function(image) {
+		var width  = image.width;
+		var height = image.height;
 
-        var c_width    = window.innerWidth;
+		var c_width	= window.innerWidth;
 
-        var this_image = document.getElementById(IDs.IMAGE_WRAP);
+		var this_image = document.getElementById(IDs.IMAGE_WRAP);
 
-        var prev_area = document.getElementById(IDs.MAINAREA_PREV_WRAP);
-        if (prev_area != undefined) {
-            prev_area.style.left   = (c_width - width) / 2 + "px";
-            prev_area.style.top    = this_image.style['margin-top'];
-            prev_area.style.width  = "200px";
-            prev_area.style.height = height + "px";
+		var prev_area = document.getElementById(IDs.MAINAREA_PREV_WRAP);
+		if (prev_area != undefined) {
+			prev_area.style.left   = (c_width - width) / 2 + "px";
+			prev_area.style.top	   = this_image.style['margin-top'];
+			prev_area.style.width  = "200px";
+			prev_area.style.height = height + "px";
 		}
 
-        var next_area = document.getElementById(IDs.MAINAREA_NEXT_WRAP);
-        if (next_area != undefined) {
-            next_area.style.right  = (c_width - width) / 2 + "px";
-            next_area.style.top    = this_image.style['margin-top'];
-            next_area.style.width  = (width - 200) + "px";
-            next_area.style.height = height + "px";
+		var next_area = document.getElementById(IDs.MAINAREA_NEXT_WRAP);
+		if (next_area != undefined) {
+			next_area.style.right  = (c_width - width) / 2 + "px";
+			next_area.style.top	   = this_image.style['margin-top'];
+			next_area.style.width  = (width - 200) + "px";
+			next_area.style.height = height + "px";
 		}
 
 
-    };
+	};
 
-    var _displayElements = function() {
-        _fadeIn(IDs.FOOTERSHADOW, 200);
-        _fadeIn(IDs.FOOTER, 200);
-        _fadeIn(IDs.MAINAREA_NEXT_WRAP, 200);
-        _fadeIn(IDs.MAINAREA_PREV_WRAP, 200);
-    };
+	var _displayElements = function() {
+		_fadeIn(IDs.FOOTERSHADOW, 200);
+		_fadeIn(IDs.FOOTER, 200);
+		_fadeIn(IDs.MAINAREA_NEXT_WRAP, 200);
+		_fadeIn(IDs.MAINAREA_PREV_WRAP, 200);
+	};
 
-    var _hideElements = function() {
-        _fadeOut(IDs.FOOTERSHADOW, 400);
-        _fadeOut(IDs.FOOTER, 400);
-        _fadeOut(IDs.MAINAREA_NEXT_WRAP, 400);
-        _fadeOut(IDs.MAINAREA_PREV_WRAP, 400);
-    };
+	var _hideElements = function() {
+		_fadeOut(IDs.FOOTERSHADOW, 400);
+		_fadeOut(IDs.FOOTER, 400);
+		_fadeOut(IDs.MAINAREA_NEXT_WRAP, 400);
+		_fadeOut(IDs.MAINAREA_PREV_WRAP, 400);
+	};
 
-    var _fadeOut = function(element_id, time_in_ms) {
-        var element = document.getElementById(element_id);
-        
-        if (element == null || element.style.display == 'none')
-        	return;
+	var _fadeOut = function(element_id, time_in_ms) {
+		var element = document.getElementById(element_id);
+		
+		if (element == null || element.style.display == 'none')
+			return;
 
-        var op = 1;
-        var timestep = 25;
-        var step_per_timestep = 1.0 / (time_in_ms / timestep);
+		var op = 1;
+		var timestep = 25;
+		var step_per_timestep = 1.0 / (time_in_ms / timestep);
 
-        var timer = setInterval(function () {
-            if (op <= step_per_timestep){
-                clearInterval(timer);
-                op = 0.0;
-                element.style.display = 'none';
-            }
-            element.style.opacity = op;
-            op -= step_per_timestep;
-        }, timestep);
-    };
+		var timer = setInterval(function () {
+			if (op <= step_per_timestep){
+				clearInterval(timer);
+				op = 0.0;
+				element.style.display = 'none';
+			}
+			element.style.opacity = op;
+			op -= step_per_timestep;
+		}, timestep);
+	};
 
-    var _fadeIn = function(element_id, time_in_ms, start_value, end_value) {
-        var element = document.getElementById(element_id);
-        
-        if (element == null || element.style.display == 'block')
-        	return;
-        
-        element.style.display = 'block';
+	var _fadeIn = function(element_id, time_in_ms, start_value, end_value) {
+		var element = document.getElementById(element_id);
+		
+		if (element == null || element.style.display == 'block')
+			return;
+		
+		element.style.display = 'block';
 
-        var op = 0.0;
-        var timestep = 25;
-        var step_per_timestep = 1.0 / (time_in_ms / timestep);
+		var op = 0.0;
+		var timestep = 25;
+		var step_per_timestep = 1.0 / (time_in_ms / timestep);
 
-        var timer = setInterval(function () {
-            if (op >= 1.0 - step_per_timestep){
-                clearInterval(timer);
-                op = 1.0;
-            }
-            element.style.opacity = op;
-            op += step_per_timestep;
-        }, timestep);
-    };
+		var timer = setInterval(function () {
+			if (op >= 1.0 - step_per_timestep){
+				clearInterval(timer);
+				op = 1.0;
+			}
+			element.style.opacity = op;
+			op += step_per_timestep;
+		}, timestep);
+	};
 
 
 
 
 	//Eventhandlers
-    var _clickEvent = function(event) {
-        if (event.target.id === IDs.BACKGROUND)
-            _closeGallery();
-    };
+	var _clickEvent = function(event) {
+		if (event.target.id === IDs.BACKGROUND)
+			_closeGallery();
+	};
 
-    var _mouseMoveEvent = function() {
-        _displayElements();
-        clearTimeout(fade_out_timeout);
-        fade_out_timeout = setTimeout(_hideElements, 2500);
-    };
+	var _mouseMoveEvent = function() {
+		_displayElements();
+		clearTimeout(fade_out_timeout);
+		fade_out_timeout = setTimeout(_hideElements, 2500);
+	};
 
-    var _prevImageEvent = function(event, overlayShownOnOpen) {	
-        if (!_currently_open)
-            return;
+	var _prevImageEvent = function(event, overlayShownOnOpen) {	
+		if (!_currently_open)
+			return;
 
-        if (_current_aid <= 0)
-            return;
+		if (_current_aid <= 0)
+			return;
 
-        _closeGallery();
-        _current_aid--;
-        _openGallery(overlayShownOnOpen);
-    };
+		_closeGallery();
+		_current_aid--;
+		_openGallery(overlayShownOnOpen);
+	};
 
-    var _nextImageEvent = function(event, overlayShownOnOpen) {		
-        if (!_currently_open)
-            return;
+	var _nextImageEvent = function(event, overlayShownOnOpen) {		
+		if (!_currently_open)
+			return;
 
-        if (_current_aid >= _galleries[_current_gid].length-1)
-            return;
+		if (_current_aid >= _galleries[_current_gid].length-1)
+			return;
 
-    	_closeGallery();
-    	_current_aid++;
-    	_openGallery(overlayShownOnOpen);
+		_closeGallery();
+		_current_aid++;
+		_openGallery(overlayShownOnOpen);
 	};
 
 
 	var _galleries = {};
-    var _offset;
+	var _offset;
 
-    var _current_gid;
-    var _current_aid;
-    var _currently_open;
+	var _current_gid;
+	var _current_aid;
+	var _currently_open;
 	
 	var _loading_image;
 
@@ -307,13 +307,13 @@ var simpleBox = (function() {
 
 
 function createElement (_type, _id, _class, _style, _append) {
-    var newElement = document.createElement(_type);
-    newElement.setAttribute("id", _id);
-    newElement.setAttribute("class", _class);
+	var newElement = document.createElement(_type);
+	newElement.setAttribute("id", _id);
+	newElement.setAttribute("class", _class);
 	newElement.setAttribute("style", _style);
-    _append.appendChild(newElement);
+	_append.appendChild(newElement);
 
-    return newElement;
+	return newElement;
 };
 
 function removeElement (_id) {
